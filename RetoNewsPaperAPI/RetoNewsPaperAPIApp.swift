7b88b15e46323834e627a5017c7c651a5a6203c4
Bryan Guapacha <bryangf0822@gmail.com>
2025-10-01 23:46:10 -0500

    Initial Commit

---
+++ RetoNewsPaperAPI/RetoNewsPaperAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetoNewsPaperAPIApp.swift
+//  RetoNewsPaperAPI
+//
+//  Created by Bryan Alexander Guapacha Florez on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RetoNewsPaperAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
